4ea1b9691c9c6090cecd15a9f26fca3c73ecad36
ZaCAS-Dep <viniciuszacariasdeoliveiractt@gmail.com>
2025-05-19 14:08:25 -0300

    CodeMotor1.ino

--- CodeMotor1.ino
+++ CodeMotor1.ino
@@ -1,28 +1,21 @@
 //Código - Robô Seguidor de Faixa:
 //Iremos fazer as definições dos pinos de controle do motor, ou seja, suas saídas e entradas, envolvendo sensores e ponte H 
-
-//Sensores
-#define SD 8 //sensor para Direita
-#define SE 9 //sensor para Esquerda
-#define SF 10 //sensor para frente
-
-//Ponte H
+#define SL 8 //sensor para esquerda
+#define SF 9 //sensor para direita
+#define SR 10 //sensor para frente
 #define IN1 7 //saída 1
 #define IN2 6 //saída 2
 #define IN3 5 //saída 3
-#define IN4 4 //saída 4 
+#define IN4 4 //saída 4- saídas ponte H
 #define ENA 11
 #define ENB 12
-
-//Inserindo variaveis
-int SensorD = 0;
-int SensorE = 0;
-int SensorF = 0; // Variável responsável por controlar a velocidade dos motores.
-int Controle = 255; // Variável de controle da velocidade
+int SensorL = 0;
+int SensorF = 0;
+int SensorR = 0; //variável responsável por controlar a velocidade dos motores.
+int Controle = 255; //Variável de controle da velocidade
 char ultimaDirecao = 'C';
 
-void ConfigDosMotores () 
-
+void ConfigDosMotores() 
 {
 //Setando os pinos de controle dos motores como saída.
 pinMode (IN1, OUTPUT);
@@ -31,12 +24,10 @@ pinMode (IN3, OUTPUT);
 pinMode (IN4, OUTPUT);
 pinMode (ENA, OUTPUT);
 pinMode (ENB, OUTPUT);
-  
 //Setando os pinos dos sensores como entrada.
-pinMode (SD, INPUT);
-pinMode (SE, INPUT);
+pinMode (SL, INPUT);
 pinMode (SF, INPUT);
-  
+pinMode (SR, INPUT);
 //Desligando enables para a segurança do robô seguidor de faixa, evitando com que ele ligue sem querer os seus motores
 digitalWrite(ENA,0);
 digitalWrite(ENB,0);
@@ -44,62 +35,70 @@ Serial.begin(9600);
 }
 
 //Criando a leitura dos sensores
-void LeituraSensores() {
-  SensorD = digitalRead(SD);
-  SensorE = digitalRead(SE);
-  SensorF = digitalRead(SF);
-  }
+void LeituraDoSensor() 
+
+{
+SensorL = digitalRead(SL);
+SensorF = digitalRead(SF);
+SensorR = digitalRead(SR);
+}
 
 //Programação para o robô ir para frente
- void RoboParaFrente() 
+void RoboParaFrente() 
+
 {
-  analogWrite(ENA, Controle*0.625); //Setando a velocidade dos motores
-  analogWrite(ENB, Controle*0.625); //Setando a velocidade dos motores
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
+analogWrite(ENA, Controle*0.625);
+analogWrite(ENB, Controle*0.625);
+digitalWrite(IN1,0);
+digitalWrite(IN2,1);
+digitalWrite(IN3,0);
+digitalWrite(IN4,1);
 }
 
 //Programação para o robô ir para Esquerda
-  void RoboParaEsquerda() 
+void RoboParaEsquerda() 
+
 {
-  analogWrite(ENA, Controle*0.625);//Setando a velocidade dos motores
-  analogWrite(ENB,Controle*0.625); //Setando a velocidade dos motores
+  analogWrite(ENA, Controle*0.625);
+  analogWrite(ENB,Controle*0.625);
   digitalWrite(IN1,0);
   digitalWrite(IN2,1);
   digitalWrite(IN3,0);
   digitalWrite(IN4,0);
-  }
+}
 
 //Programação para o robô ir para Direita
-  void RoboParaDireita() 
+void RoboParaDireita() 
 {
-    analogWrite(ENA, Controle*0.625); //Setando a velocidade dos motores
-    analogWrite(ENB,Controle*0.625); //Setando a velocidade dos motores
-    digitalWrite(IN1,0);
-    digitalWrite(IN2,0);
-    digitalWrite(IN3,0);
-    digitalWrite(IN4,1);
-  }
+analogWrite(ENA, Controle*0.625);
+analogWrite(ENB,Controle*0.625);
+digitalWrite(IN1,0);
+digitalWrite(IN2,0);
+digitalWrite(IN3,0);
+digitalWrite(IN4,1);
+}
 
 //Lógica para o robô seguir as curvas
-    void ControleDoRobo() 
+void ControleDoRobo() 
 {
-    if(SensorD == 1 && SensorE == 0 && SensorF == 1)  { RoboParaEsquerda(); } // 0 = Branca (Sem reflexão de luzes) 1 = Preto (Reflete luz para o sensor)
-    if(SensorD == 0 && SensorE == 1 && SensorF == 1)  { RoboParaDireita(); }
-    if(SensorD == 1 && SensorE == 1 && SensorF == 0)  { RoboParaFrente(); }
+    if(SensorL == 1 && SensorF == 0 && SensorR == 1)  { RoboParaFrente(); }
+    if(SensorL == 0 && SensorF == 1 && SensorR == 1)  { RoboParaEsquerda(); }
+    if(SensorL == 1 && SensorF == 1 && SensorR == 0)  { RoboParaDireita(); }
 }
 
   
     
-   
-    void setup () 
+//Colocando a lógica de início   
+void setup () 
 {
-    ConfigDosMotores();
+ConfigDosMotores();
 }
-    void loop() 
+
+//Colocando o sistema de vistória e aplicando a lógica de movimento
+void loop()
 {
-      LeituraSensores();
-      ControleDoRobo();
+LeituraDoSensor();
+ControleDoRobo();
 }
+
+//Utilizando essa lógica e a montagem correta conseguimos concluir o percuso com o robô segue faixa.
