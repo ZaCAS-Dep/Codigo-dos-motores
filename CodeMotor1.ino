31970dee3c86b083e31e1c24a625c9e1b66abf14
ZaCAS-Dep <viniciuszacariasdeoliveiractt@gmail.com>
2025-05-19 14:28:55 -0300

    CodeMotor1.ino

--- CodeMotor1.ino
+++ CodeMotor1.ino
@@ -1,4 +1,4 @@
-//Código - Robô Seguidor de Faixa:
+//Olá, iremos fazer um código com o uso da programação no Arduíno IDE para o robô segue faixa:
 //Iremos fazer as definições dos pinos de controle do motor, ou seja, suas saídas e entradas, envolvendo sensores e ponte H 
 #define SL 8 //sensor para esquerda
 #define SF 9 //sensor para direita
@@ -6,25 +6,25 @@
 #define IN1 7 //saída 1
 #define IN2 6 //saída 2
 #define IN3 5 //saída 3
-#define IN4 4 //saída 4- saídas ponte H
-#define ENA 11
-#define ENB 12
+#define IN4 4 //saída 4
+#define ENA 11 //controla a velocidade do motor pela ponte H
+#define ENB 12 //controla a velocidade do motor pela ponte H
 int SensorL = 0;
 int SensorF = 0;
-int SensorR = 0; //variável responsável por controlar a velocidade dos motores.
+int SensorR = 0; 
 int Controle = 255; //Variável de controle da velocidade
 char ultimaDirecao = 'C';
 
 void ConfigDosMotores() 
 {
-//Setando os pinos de controle dos motores como saída.
+//Setando os pinos de controle dos motores como saídas.
 pinMode (IN1, OUTPUT);
 pinMode (IN2, OUTPUT);
 pinMode (IN3, OUTPUT);
 pinMode (IN4, OUTPUT);
 pinMode (ENA, OUTPUT);
 pinMode (ENB, OUTPUT);
-//Setando os pinos dos sensores como entrada.
+//Setando os pinos dos sensores como entradas.
 pinMode (SL, INPUT);
 pinMode (SF, INPUT);
 pinMode (SR, INPUT);
@@ -47,7 +47,7 @@ SensorR = digitalRead(SR);
 void RoboParaFrente() 
 
 {
-analogWrite(ENA, Controle*0.625);
+analogWrite(ENA, Controle*0.625);//usando 62,5% da velocidade 255 dos motores
 analogWrite(ENB, Controle*0.625);
 digitalWrite(IN1,0);
 digitalWrite(IN2,1);
@@ -59,8 +59,8 @@ digitalWrite(IN4,1);
 void RoboParaEsquerda() 
 
 {
-  analogWrite(ENA, Controle*0.625);
-  analogWrite(ENB,Controle*0.625);
+  analogWrite(ENA, Controle*0.625);//usando 62,5% da velocidade 255 dos motores
+  analogWrite(ENB,Controle*0.625);//usando 62,5% da velocidade 255 dos motores
   digitalWrite(IN1,0);
   digitalWrite(IN2,1);
   digitalWrite(IN3,0);
@@ -70,15 +70,15 @@ void RoboParaEsquerda()
 //Programação para o robô ir para Direita
 void RoboParaDireita() 
 {
-analogWrite(ENA, Controle*0.625);
-analogWrite(ENB,Controle*0.625);
+analogWrite(ENA, Controle*0.625);//usando 62,5% da velocidade 255 dos motores
+analogWrite(ENB,Controle*0.625);//usando 62,5% da velocidade 255 dos motores
 digitalWrite(IN1,0);
 digitalWrite(IN2,0);
 digitalWrite(IN3,0);
 digitalWrite(IN4,1);
 }
 
-//Lógica para o robô seguir as curvas
+//Lógica para o robô seguir as curvas de forma correta
 void ControleDoRobo() 
 {
     if(SensorL == 1 && SensorF == 0 && SensorR == 1)  { RoboParaFrente(); }
